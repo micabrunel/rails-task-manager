1642d8c4262ddceae58d57ae5d77bf61bc1f5ce9
Michael Brunel <brunel.michael@gmail.com>
2019-08-06 12:59:45 +0200

    index and show done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new'
+  post 'task/create', to: 'tasks#create'
+  get 'task/:id', to: 'tasks#show', as: :task
+
+  get 'task/:id/edit', to: 'tasks#edit'
+  patch 'task/:id', to: 'tasks#update'
+
+  delete 'task/:id', to: 'tasks#destroy'
+
 end
